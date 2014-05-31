4a55ae13f3a2f69ec5d20512d483e6336ac63c7b
s29470352 <alicechen0331@gmail.com>
2014-05-31 21:39:40 +0800

    v01

---
+++ src/com/example/asgard/card01.java
@@ -0,0 +1,12 @@
+package com.example.asgard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class card01 extends Activity{
+	  @Override
+	   protected void onCreate(Bundle savedInstanceState) {
+	       super.onCreate(savedInstanceState);
+	       setContentView(R.layout.card01);
+	  }
+}
